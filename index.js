dec9ff6ce74d8c2059b533a3f6b0e3f75cab420b
Tim Schaub <tim.schaub@gmail.com>
2016-12-27 14:14:55 -0700

    Rule to enforce shared modules for enums

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   rules: {
+    'enum': require('./enum').rule,
     'no-duplicate-requires': require('./no-duplicate-requires').rule,
     'no-missing-requires': require('./no-missing-requires').rule,
     'no-unused-requires': require('./no-unused-requires').rule,
