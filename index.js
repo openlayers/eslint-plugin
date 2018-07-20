91eed09dc99b36c29ca809286fcb200f5cafef0f
Tim Schaub <tim.schaub@gmail.com>
2018-07-20 10:31:42 -0600

    New rule

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   rules: {
-    'enum': require('./enum').rule,
-    'no-duplicate-requires': require('./no-duplicate-requires').rule,
-    'no-missing-requires': require('./no-missing-requires').rule,
-    'no-unused-requires': require('./no-unused-requires').rule,
-    'one-provide': require('./one-provide').rule,
-    'requires-first': require('./requires-first').rule,
-    'valid-provide': require('./valid-provide').rule,
-    'valid-requires': require('./valid-requires').rule
+    'valid-tsdoc': require('./valid-tsdoc').rule
   }
 };
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Custom ESLint Rules for OpenLayers
 
-This package is used as an ESLint plugin for the OpenLayers repository.  The rules are meant to help with the transition away from the Closure Library and may be removed when that is complete.
+This package is used as an ESLint plugin for the OpenLayers repository.  The rules should be temporary as we wait for fixes in other packages.
+
